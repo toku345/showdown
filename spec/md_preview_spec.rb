54976cf74356bb420e2c74b4d4e3a1651b7f9acf
Fumitaka Tokumitsu <toku345@gmail.com>
2014-07-19 16:00:05 +0900

    Add MdPreview::Converter spec

--- spec/md_preview_spec.rb
+++ spec/md_preview_spec.rb
@@ -4,8 +4,36 @@ describe MdPreview do
   it 'has a version number' do
     expect(MdPreview::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe MdPreview::RackApp do
+end
+
+describe MdPreview::Converter do
+  include MdPreview::Converter
+
+  describe '#converted_html' do
+    before(:all) { @target_path = File.expand_path('../support/test.md', __FILE__) }
+
+    let(:expected_html) { "<p>This is <em>bongos</em>, indeed.</p>\n" }
+
+    context '@markdown is nil' do
+      before(:each) { @markdown = nil }
+
+      it 'return converted html' do
+        expect(converted_html).to eq expected_html
+      end
+    end
+
+    context '@markdown is not nil' do
+      before(:all) { @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML) }
+
+      it 'return converted html' do
+        expect(converted_html).to eq expected_html
+      end
+    end
   end
+end
+
+describe MdPreview::WatchDog do
 end
